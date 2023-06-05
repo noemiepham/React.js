4892acdffce96fd5d4091829940cb8ae8090c9ea
Noémie Huynh <huynhtieuquy@gmail.com>
2023-06-05 14:32:37 +0200

    mapping components react

--- keeper-app/src/components/Note.jsx
+++ keeper-app/src/components/Note.jsx
@@ -6,6 +6,7 @@ function Note(){
           <div className="note">
                <h1>TodoList</h1>
                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, e?</p>
+               
           </div>
      )
 }
